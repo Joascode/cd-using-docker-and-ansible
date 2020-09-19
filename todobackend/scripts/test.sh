1f2a73ed701b2dca0868639858f69ce42521dbb2
Joas Kramer <joaskramer@outlook.com>
2020-09-19 20:46:07 +0200

    Update docker-compose to include build step
    This build step sets up the Python application as a wheel package, ready to be used for acceptance testing and eventually as release candidate. This step required updates to test.sh script to download pip packages first before installing, so they could be used by both the test and build image creation steps. This makes both images the same, as they use the same downloaded packages saved in the cache.

--- todobackend/scripts/test.sh
+++ todobackend/scripts/test.sh
@@ -2,8 +2,15 @@
 # Activate virtual environment
 . /appenv/bin/activate
 
+# Download requirements to build cache
+# Since the requirements_text.txt file references the setup.py file, pip will
+# also place a copy of the application source code in the /build folder.
+pip download -d /build -r requirements_test.txt --no-input
+
 # Install application test requirements
-pip install -r requirements_test.txt
+# The --no-index flags tells pip install to not download the external
+# depedencies and instead use the /build folder.
+pip install --no-index -f /build -r requirements_test.txt
 
 # Run test.sh arguments
 exec $@
